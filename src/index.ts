87a7201e92da30954c906c0b8846b461abae6ba3
Steven Barquet <mlmjm123@gmail.com>
2021-12-16 11:22:34 -0600

    hr 5

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,35 @@
 /* eslint-disable no-return-assign */
 
-// Source: https://www.hackerrank.com/challenges/diagonal-difference/problem?isFullScreen=true
+// Source: https://www.hackerrank.com/challenges/plus-minus/problem?isFullScreen=true
 
-function diagonalDifference(arr: number[][]): number {
+function plusMinus(arr: number[]): void {
   // Write your code here
-  const n = arr.length;
-  let diag1 = 0; // 4
-  let diag2 = 0;
-  for (let i = 0; i < n; i++) {
-    // i = 3
-    for (let j = 0; j < n; j++) {
-      // j = 3
-      if(i === j){
-        diag1=diag1+arr[i][j]
-      }
-      // diag1= 4
+  const { length } = arr
+  let positives = 0;
+  let negative = 0;
+  let neutrals = 0;
+
+  for (let i = 0; i < length; i++) {
+    const element = arr[i];
+    if( element > 0){
+      positives++
+    } else if( element < 0 ){
+      negative++
+    } else {
+      neutrals++
     }
   }
-  for (let i = 0; i < n; i++) {
-    // i = 2
-    for (let j = 0; j < n; j++) {
-      // j = 0
-      if(i + j === n-1){
-        diag2=diag2+arr[i][j]
-      }
-      // diag2 = 19
-    }
-  }
+
+  console.log('Positives: ', positives/length);
+  console.log('Negative: ', negative/length);
+  console.log('Neutrals: ', neutrals/length);
   
-  return Math.abs(diag2-diag1)
 }
 
 function main() {
   console.log('---------- Cool Programing ----------\n');
-  const x = [11,2,4]
-  const y = [4,5,6]
-  const z = [10,8,-12]
-  const a = [x, y, z] // (a[0])[1]
-  const result = diagonalDifference(a);
+  const a = [-4, 3, -9, 0, 4, 1]
+  const result = plusMinus(a);
   console.log('Result: ', result);
 }
 
