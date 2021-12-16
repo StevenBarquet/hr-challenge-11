4c7d8dd3acf2483e5418890afb4ace32577b8934
Steven Barquet <mlmjm123@gmail.com>
2021-12-16 11:02:15 -0600

    challange 4

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,42 @@
 
 // Source: https://www.hackerrank.com/challenges/compare-the-triplets/problem?isFullScreen=true
 
-function compareTriplets(a: number[], b: number[]): number[] {
+function diagonalDifference(arr: number[][]): number {
   // Write your code here
-  let aPoints = 0;
-  let bPoints = 0;
-  for (let index = 0; index < a.length; index++) {
-      if(a[index]>b[index]){
-        aPoints++
-      } else if(b[index]>a[index]){
-        bPoints++
+  const n = arr.length;
+  let diag1 = 0; // 4
+  let diag2 = 0;
+  for (let i = 0; i < n; i++) {
+    // i = 3
+    for (let j = 0; j < n; j++) {
+      // j = 3
+      if(i === j){
+        diag1=diag1+arr[i][j]
       }
+      // diag1= 4
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    // i = 2
+    for (let j = 0; j < n; j++) {
+      // j = 0
+      if(i + j === n-1){
+        diag2=diag2+arr[i][j]
+      }
+      // diag2 = 19
+    }
   }
   
-  return [aPoints,bPoints];
+  return Math.abs(diag2-diag1)
 }
 
 function main() {
   console.log('---------- Cool Programing ----------\n');
-  const a = [1, 2, 3]
-  const b = [3, 2, 1]
-  const result = compareTriplets(a, b);
+  const x = [11,2,4]
+  const y = [4,5,6]
+  const z = [10,8,-12]
+  const a = [x, y, z] // (a[0])[1]
+  const result = diagonalDifference(a);
   console.log('Result: ', result);
 }
 
