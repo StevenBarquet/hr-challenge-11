e622c0e719c08797acc914f31837e44a4c5dbe5f
Steven Barquet <mlmjm123@gmail.com>
2021-12-21 13:50:16 -0600

    hr 11

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,54 @@
 /* eslint-disable no-return-assign */
 
-// Source: https://www.hackerrank.com/challenges/grading/problem?isFullScreen=true
+// Source: https://www.hackerrank.com/challenges/apple-and-orange/problem?isFullScreen=true
 
-function gradingStudents(grades: number[]): number[] {
+function countApplesAndOranges(
+  s: number,
+  t: number,
+  a: number,
+  b: number,
+  apples: number[],
+  oranges: number[]
+): void {
   // Write your code here
-
-  function toRound(grade: number): number {
-    const next5 = 5 - (grade % 5);
-    const willRound = grade >= 38 && next5 < 3;
-    if (willRound) {
-      return grade + next5;
-    }
-    return grade;
+  function calculateInRange(
+    fruits: number[],
+    start: number,
+    end: number,
+    treePosition: number
+  ): number {
+    let counter = 0;
+    fruits.forEach((element) => {
+      const fruitPosition = treePosition + element;
+      const isInRange = fruitPosition >= start && fruitPosition <= end;
+      if (isInRange) {
+        counter++;
+      }
+    });
+    return counter;
   }
-  const fixedGrades = grades.map((element) => toRound(element));
-
-  return fixedGrades;
+  const applesInRange = calculateInRange(apples, s, t, a);
+  const orangesInRange = calculateInRange(oranges, s, t, b);
+  console.log(applesInRange);
+  console.log(orangesInRange);
 }
 
 function main() {
   console.log('---------- Cool Programing ----------\n');
-  const a = [73, 67, 45, 33];
-  const result = gradingStudents(a);
+  const s = 7;
+  const t = 11;
+  const appleTree = 5;
+  const orangeTree = 15;
+  const apples = [-2, 2, 1];
+  const oranges = [5, -6];
+  const result = countApplesAndOranges(
+    s,
+    t,
+    appleTree,
+    orangeTree,
+    apples,
+    oranges
+  );
   console.log('Result: ', result);
 }
 
